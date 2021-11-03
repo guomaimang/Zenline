883579542fa401bf0cf669f333aa5079e0a1ebd0
SHUkunxin <77830989+SHUkunxin@users.noreply.github.com>
2021-11-04 00:40:22 +0800

    up d

--- clevis/src/hk/edu/polyu/comp/comp2021/clevis/model/ShellText.java
+++ clevis/src/hk/edu/polyu/comp/comp2021/clevis/model/ShellText.java
@@ -1,7 +1,7 @@
 package hk.edu.polyu.comp.comp2021.clevis.model;
 
 public class ShellText {
-    String graphName;
+    String shapeName;
     String collectionName;
     Action actionType;
     Point point1;
@@ -12,12 +12,43 @@ public class ShellText {
     Double Distance3;
 
     public ShellText(String shell){
+        String num1 = "";
+        String num2 = "";
+        String num3 = "";
+        String num4 = "";
+        String num5 = "";
+        String action = "";
+
+        String[] stringArr = shell.split("\\s");
+        for (int i = 0; i < stringArr.length; i++) {
+            switch (i){
+                case 0:
+                    action = stringArr[i];
+                    break;
+                case 1:
+                    num1 = stringArr[i];
+                    break;
+                case 2:
+                    num2 = stringArr[i];
+                    break;
+                case 3:
+                    num3 = stringArr[i];
+                    break;
+                case 4:
+                    num4 = stringArr[i];
+                    break;
+                case 5:
+                    num5 = stringArr[i];
+                    break;
+            }
+        }
         
-        Action ac = Action.valueOf(shell);
-        switch(ac){
+        Action at = Action.valueOf(action);
+        switch(at){
             case CIRCLE:
                 System.out.println("111");
                 actionType = Action.CIRCLE;
+                point1 =
                 break;
             case LINE:
                 System.out.println("222");
