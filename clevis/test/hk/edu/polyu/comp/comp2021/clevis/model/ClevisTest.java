aa969a95c63395cac7d39906f04b2bc5ff655dd3
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2021-11-14 20:37:28 +0800

    Update ClevisTest.java

--- clevis/test/hk/edu/polyu/comp/comp2021/clevis/model/ClevisTest.java
+++ clevis/test/hk/edu/polyu/comp/comp2021/clevis/model/ClevisTest.java
@@ -2,13 +2,23 @@ package hk.edu.polyu.comp.comp2021.clevis.model;
 
 // import org.junit.Test;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class ClevisTest {
 
+    @BeforeAll
+    //
+
+    @Test
+    void testa(){
+
+    }
     @Test
     public void testClevisConstructor(){
 
     }
+
+
 
 }
